8441f93fb779d6270fe7710acd509533c1b0b818
BestByte <ilvxna@hotmail.com>
2019-07-29 06:09:42 +0800

    add webview

--- app/src/main/java/com/cool/andoroidall/MainActivity.java
+++ app/src/main/java/com/cool/andoroidall/MainActivity.java
@@ -3,6 +3,8 @@ package com.cool.andoroidall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView=(WebView)findViewById(R.id.web_view);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.baidu.com");
     }
 }
