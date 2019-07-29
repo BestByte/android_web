2d84cb1da8debca5e683be51f633aca68c2ac694
BestByte <ilvxna@hotmail.com>
2019-07-29 10:24:13 +0800

    add view java

--- app/src/main/java/com/cool/andoroidall/MainActivity.java
+++ app/src/main/java/com/cool/andoroidall/MainActivity.java
@@ -3,22 +3,35 @@ package com.cool.andoroidall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView responseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView=(WebView)findViewById(R.id.web_view);
+        Button button=(Button) findViewById(R.id.send_request);
 
-        webView.getSettings().setJavaScriptEnabled(true);
+        responseText=(TextView)findViewById(R.id.response_text);
 
-        webView.setWebViewClient(new WebViewClient());
+        button.setOnClickListener(this);
+    }
 
-        webView.loadUrl("https://www.baidu.com/");
+    @Override
+    public  void  onClick(View v) {
+        if (v.getId() == R.id.send_request) {
+            sendRequestWithHttpUrlConnection();
+        }
+    }
+    private void sendRequestWithHttpUrlConnection(){
+
     }
 }
